c1964a397dd3f48b4906b5eb02a11449a4b0a785
Ryan Fredlund <rfredlund13@gmail.com>
2016-05-18 19:03:48 -0500

    Implemented Ramith's suggestions

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ extern crate git2;
 extern crate rustc_serialize;
 extern crate docopt;
 
+use std::env;
 use std::path::Path;
 use docopt::Docopt;
 use proton_cli::Error;
@@ -15,7 +16,7 @@ Command-line interface for Proton
 
 Usage:
   ./proton init <folder>
-  ./proton user new <public> <name>
+  ./proton new-user <folder> <public-key> <name>
   ./proton (-h | --help)
 
 Options:
@@ -25,10 +26,9 @@ Options:
 #[derive(Debug, RustcDecodable)]
 struct Args {
 	cmd_init: bool,
+	cmd_new_user: bool,
 	arg_folder: Option<String>,
-	cmd_user: bool,
-	cmd_new: bool,
-	arg_public: Option<String>,
+	arg_public_key: Option<String>,
 	arg_name: Option<String>,
 }
 
@@ -38,26 +38,32 @@ fn main() {
 		.and_then(|d| d.decode())
 		.unwrap_or_else(|e| e.exit());
 
-	let mut result: Result<(), Error> = Err(Error::ArgumentNotFound);
+	let command: fn(Args) -> Result<(), Error> = match env::args().nth(1).unwrap().as_ref() { // Safe because of docopt
+		"init" => run_init,
+		"new-user" => run_new_user,
+		_ => panic!("Invalid first argument"),
+	};
 
-	if args.cmd_init {
-		let root = args.arg_folder.expect("Folder cannot be 'None'");
-		let root_path = Path::new(&root);
-		result = proton_cli::initialize_project(&root_path);
-	} else if args.cmd_user {
-		if args.cmd_new {
-			let public_key = args.arg_public.expect("Public key cannot be 'None'");
-			let public_key_path = Path::new(&public_key);
-			let name = args.arg_name.expect("Name cannot be 'None'");
-			println!("{}, {}", public_key, name);
-			result = Ok(());
-			//result = proton_cli::user_new(&public_key_path, &name);
-		}
-	}
-
+	let result = command(args);
 	match result {
 		Ok(_) => println!("Worked!"),
 		Err(e) => println!("{:?}", e.to_string()),
-	}
+	};
 
+}
+
+fn run_init(args: Args) -> Result<(), Error> {
+	let root = args.arg_folder.unwrap(); // Safe because of docopt
+	let root_path = Path::new(&root);
+	proton_cli::initialize_project(&root_path)
+}
+
+fn run_new_user(args: Args) -> Result<(), Error> {
+	let folder = args.arg_folder.unwrap(); // Safe because of docopt
+	let public_key = args.arg_public_key.unwrap(); // Safe because of docopt
+	let public_key_path = Path::new(&public_key);
+	let name = args.arg_name.unwrap(); // Safe because of docopt
+	println!("{}, {}, {}", folder, public_key, name);
+	Ok(())
+	//proton_cli::add_user(folder, public_key_path, name)
 }
