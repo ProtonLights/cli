3cce1c4c9ba6190799c7733b893f2510425267a2
Ryan Fredlund <rfredlund13@gmail.com>
2016-05-19 20:03:31 -0500

    Simplified comments

--- src/main.rs
+++ src/main.rs
@@ -38,7 +38,9 @@ fn main() {
 		.and_then(|d| d.decode())
 		.unwrap_or_else(|e| e.exit());
 
-	let command: fn(Args) -> Result<(), Error> = match env::args().nth(1).unwrap().as_ref() { // Safe because of docopt
+	// Below unwrap()'s are safe within Docopt's usage rules
+
+	let command: fn(Args) -> Result<(), Error> = match env::args().nth(1).unwrap().as_ref() {
 		"init" => run_init,
 		"new-user" => run_new_user,
 		_ => panic!("Invalid first argument"),
@@ -53,16 +55,16 @@ fn main() {
 }
 
 fn run_init(args: Args) -> Result<(), Error> {
-	let root = args.arg_folder.unwrap(); // Safe because of docopt
+	let root = args.arg_folder.unwrap();
 	let root_path = Path::new(&root);
 	proton_cli::initialize_project(&root_path)
 }
 
 fn run_new_user(args: Args) -> Result<(), Error> {
-	let folder = args.arg_folder.unwrap(); // Safe because of docopt
-	let public_key = args.arg_public_key.unwrap(); // Safe because of docopt
+	let folder = args.arg_folder.unwrap();
+	let public_key = args.arg_public_key.unwrap();
 	let public_key_path = Path::new(&public_key);
-	let name = args.arg_name.unwrap(); // Safe because of docopt
+	let name = args.arg_name.unwrap();
 	println!("{}, {}, {}", folder, public_key, name);
 	Ok(())
 	//proton_cli::add_user(folder, public_key_path, name)
