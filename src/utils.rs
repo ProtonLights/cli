5f43427098ee6c07cb98f315a3a30f964ff0dd3b
Ryan Fredlund <rfredlund13@gmail.com>
2016-05-21 14:39:52 -0500

    Trim string read from file

--- src/utils.rs
+++ src/utils.rs
@@ -36,7 +36,7 @@ pub fn file_as_string<P: AsRef<Path>>(path: P) -> Result<String, Error> {
         .and_then(|mut file| {
             let mut string = String::new();
             file.read_to_string(&mut string)
-                .and_then(|_| Ok(string))           
+                .and_then(|_| Ok(string.trim().to_string()))           
         })
         .map_err(Error::Io)
 }
